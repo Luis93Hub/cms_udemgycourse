37e45b5b46cf4cc02aacfe14d9bef429291c1a32
luis <luis93hernan2@gmail.com>
2023-03-21 21:01:35 -0600

    Adding all folder and file course udemy

---
+++ includes/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$db['db_host'] = "db";
+$db['db_user'] = "root";
+$db['db_password'] = "root";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value){
+define(strtoupper($key), $value);
+}
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+// if (!$connection) {
+//     echo 'no se pudo conectar a la base de datos';
+// } else {
+//     echo "Genial esto esta conectado";
+// }
+

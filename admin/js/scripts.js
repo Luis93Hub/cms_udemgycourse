6c179e0262ff46530d0d941180073c5da6b4fdba
luis <luis93hernan2@gmail.com>
2023-07-15 12:08:30 -0600

    adding Bulk Options All posts

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
   $('#summernote').summernote();
 });
+
+$(document).ready(function () {
+  $('#selectAllBoxes').click(function (event) {
+    if (this.checked) {
+      $('.checkBoxes').each(function () {
+        this.checked = true;
+      });
+    } else {
+      $('.checkBoxes').each(function () {
+        this.checked = false;
+      });
+    }
+  });
+
+});

476893b534a9223b94435df321df7a56d66c45b6
luis <luis93hernan2@gmail.com>
2023-07-19 12:33:11 -0600

    adding pager en pagerCss

--- index_old.php
+++ index_old.php
@@ -16,29 +16,10 @@ include 'includes/db.php'; ?>
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">;
+            <div class="col-md-8">
 
             <?php
-
-            $per_page = 5;
-
-            if (isset($_GET['page'])) {
-                $page = $_GET['page'];
-            } else {
-                $page = "";
-            }
-            if ($page == "" || $page == 1) {
-                $page_1 = 0;
-            } else {
-                $page_1 = ($page * $per_page ) - $per_page ;
-            }
-
-            $post_query_count = "SELECT * FROM posts";
-            $find_count = mysqli_query($connection, $post_query_count);
-            $count = mysqli_num_rows($find_count);
-            $count = ceil($count / 5);
-
-            $query = "SELECT * FROM posts LIMIT $page_1, $per_page ";
+            $query = "SELECT * FROM posts";
             $select_all_posts_query = mysqli_query($connection, $query);
 
 
@@ -90,17 +71,6 @@ include 'includes/db.php'; ?>
 <!-- /.row -->
 
 <hr>
-<ul class="pager">
-    <?php
-    for ($i = 1; $i <= $count; $i++) {
-        if ($i == $page) {
-            echo "<li '><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-        } else {
-            echo "<li '><a href='index.php?page={$i}'>{$i}</a></li>";
-        }
-    }
-    ?>
-</ul>
 
 
     <?php include 'includes/footer.php';

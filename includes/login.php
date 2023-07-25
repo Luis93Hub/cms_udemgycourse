4ef374ab817eff16bac127839904ed2fb3de75bd
luis <luis93hernan2@gmail.com>
2023-07-24 21:49:02 -0600

    Adding password verify has

--- includes/login.php
+++ includes/login.php
@@ -9,7 +9,7 @@ if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($connection, $username);
     $password = mysqli_real_escape_string($connection, $password);
 
-    $query = "SELECT * FROM users WHERE username = '{$username}'";
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
     $select_user_query = mysqli_query($connection, $query);
     if (!$select_user_query) {
         die('QUERY FAILED' . mysqli_error($connection));
@@ -24,9 +24,9 @@ if (isset($_POST['login'])) {
          $db_user_role = $row ['user_role'];
     }
 
-    $password = crypt($password, $db_user_password);
+    // $password = crypt($password, $db_user_password);
 
-    if ($username === $db_username && $password === $db_user_password) {
+    if (password_verify($password, $db_user_password)) {
         $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
         $_SESSION['lastname'] = $db_user_lastname;

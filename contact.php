5d5a3fd06b672c523c91a867ea91636ce0b50cc5
Luis_Hernandez <102109226+Luis93Hub@users.noreply.github.com>
2024-07-29 01:00:23 +0300

    Update contact.php
    Deleting credentials

--- contact.php
+++ contact.php
@@ -14,8 +14,8 @@
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'luis93hernan2@gmail.com';
-    $mail->Password = 'joefdwqiuwdqzhsy';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
